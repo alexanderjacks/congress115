e37808861e0e9e94d8aae9097d11d3103f4c2b95
Adjectival <alexd.jacks@gmail.com>
2018-01-28 13:26:35 -0800

    :art: initial styling of widget text + cleans up header

--- src/App.js
+++ src/App.js
@@ -1,39 +1,55 @@
 import React, { Component } from 'react';
 import { Router, browserHistory, Route, Link } from 'react-router';
-import { Button, ButtonGroup, DropdownButton, MenuItem } from 'react-bootstrap';
+import {  } from 'react-bootstrap';
 import FontAwesome from 'react-fontawesome';
 
 import './App.css';
 
 const Page = ({ title }) => (
     <div className="App">
-      <div className="App-header row justify-content-center">
-          <h2 className="emboss">F<FontAwesome
-            name='cog'
-            className='fa-1x emboss'
-            spin
-          />nt Awes<FontAwesome
-            name='cog'
-            className='fa-1x emboss'
-            spin
-          />
-      me Crypt<FontAwesome
+
+{/* title, gears for Ohs */}
+<div className="App-header row justify-content-center">
+
+<h1 className="emboss">
+    {/* line 1 */}
+    <span className="d-block">F<FontAwesome
         name='cog'
         className='fa-1x emboss'
         spin
-      />  N<FontAwesome
-            name='cog'
-            className='fa-1x emboss'
-            spin
-          />
-            tes</h2>
-      </div>
+    />nt Awes<FontAwesome
+        name='cog'
+        className='fa-1x emboss'
+        spin
+    />
+    me
+    </span>
+    {/* line 2 */}
+    <span className="d-block">Crypt<FontAwesome
+        name='cog'
+        className='fa-1x emboss'
+        spin
+    />  N<FontAwesome
+        name='cog'
+        className='fa-1x emboss'
+        spin
+    />
+    tes
+    </span>
+</h1>
+
+</div>
+
+{/* quips, under dev */}
 
       <p className="App-intro mt-3">
-        Another splendid hybrid
+        Spinning gears too much?
       </p>
 
-      <h1 className="center">Font Awesome Notes on Crypto</h1>
+      <h1 className="center">Font Awesome Crypto Notes</h1>
+      <h3 className="center">Icon key will go here, in small table</h3>
+
+{/* begin list of widgets */}
 
       <div className="coinmarketcap-currency-widget b1" data-currency="bitcoin" data-base="ETH" data-secondary="USD" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="ETH" data-statsticker="false"></div>
 
@@ -49,9 +65,10 @@ const Page = ({ title }) => (
 
       <div className="coinmarketcap-currency-widget b7" data-currency="litecoin" data-base="ETH" data-secondary="USD" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="ETH" data-statsticker="false"></div>
 
-
       <div className="coinmarketcap-currency-widget b9" data-currency="eos" data-base="ETH"  data-secondary="BTC"></div>
+
       <div className="coinmarketcap-currency-widget b8" data-currency="neo" data-base="ETH"  data-secondary="BTC"></div>
+
       <div className="coinmarketcap-currency-widget b10" data-currency="nem" data-base="ETH" data-secondary="USD" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
 
       <div className="coinmarketcap-currency-widget b14" data-currency="iota" data-base="ETH" data-secondary="USD" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
@@ -62,63 +79,40 @@ const Page = ({ title }) => (
 
       <div className="coinmarketcap-currency-widget b11" data-currency="tron" data-base="ETH" data-secondary="USD" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
 
-      <h2>Card Deck Bounces Now... But Enough, Yes?
-      </h2>
-      <div className="card-deck">
-  <div className="card p-2 m-3">
-    <div className="card-body">
-      <h4 className="card-title dramatic-drop-shadow">Prestigiditation</h4>
-      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    </div>
-  </div>
-  <div className="card p-2 m-3">
-    <div className="card-body">
-      <h4 className="card-title dramatic-drop-shadow">Legerdemain</h4>
-      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-    </div>
-  </div>
-  <div className="card p-2 m-3">
-    <div className="card-body">
-      <h4 className="card-title dramatic-drop-shadow">Ensorcellment</h4>
-      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-    </div>
-  </div>
-</div>
-
-      <h6>Font Awesome Crypto Notes
-      </h6>
-      <p className="row justify-content-around">
-        <Link className="fire" to="/">Home</Link>
-        <a href="https://www.alexanderjacks.com"><span className="fire"> Author</span></a>
-        <Link className="fire" to="/settings">Settings</Link>
-      </p>
-    </div>
+     <h6>Font Awesome Crypto Notes
+     </h6>
+     <p className="row justify-content-around">
+       <Link className="fire" to="/">Home</Link>
+       <a href="https://www.alexanderjacks.com"><span className="fire"> Author</span></a>
+       <Link className="fire" to="/settings">Settings</Link>
+     </p>
+   </div>
 );
 
 
 
 const Home = (props) => (
-  <Page title="Home"/>
+ <Page title="Home"/>
 );
 
 const About = (props) => (
-  <Page title="About"/>
+ <Page title="About"/>
 );
 
 const Settings = (props) => (
-  <Page title="Settings"/>
+ <Page title="Settings"/>
 );
 
 class App extends Component {
-  render() {
-    return (
-      <Router history={browserHistory}>
-        <Route path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/settings" component={Settings}/>
-      </Router>
-    );
-  }
+ render() {
+   return (
+     <Router history={browserHistory}>
+       <Route path="/" component={Home}/>
+       <Route path="/about" component={About}/>
+       <Route path="/settings" component={Settings}/>
+     </Router>
+   );
+ }
 }
 
 
