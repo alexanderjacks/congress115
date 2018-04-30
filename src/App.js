1710160a3414a9e0c8bf1d1eb52ffe020758d55d
Adjectival <alexd.jacks@gmail.com>
2018-04-30 09:32:36 -0700

    :sparkles::horse: dropdown menu for filters

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
-import { Crypto } from './Crypto';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'mdbreact';
 
 import './App.css';
 
@@ -10,7 +10,9 @@ class App extends React.Component {
 
     constructor(props) {   
         super(props)
-        this.state = { coins: [] }
+        this.state = {
+            coins: []
+        }
         this.sortByRankDesc = this.sortByRankDesc.bind(this);
         this.sortByPriceAsc = this.sortByPriceAsc.bind(this);
         this.sortByChangeHour = this.sortByChangeHour.bind(this);
@@ -23,7 +25,12 @@ class App extends React.Component {
             .then(res => {
                 const coins = res.data;
                 this.setState({ coins });
-            })
+            });
+        axios.get()
+            .then(res => {
+                const history = res.data;
+                this.setState({ history });
+            });
     }
 
     sortByRankDesc() {
@@ -60,7 +67,7 @@ class App extends React.Component {
     render() {
         return (
         <div className="">
-            <div className="header-shading p-3 text-center">
+            <div className="header-shading p-3 text-left" id="top">
 
                 <h1 className="display-5">Top 99 Crypto Currencies</h1>
                 <h2 className="display-6">Updated every 5 minutes <a href="https://coinmarketcap.com/all/views/all/">via</a></h2>
@@ -79,44 +86,46 @@ class App extends React.Component {
                 </h6>
 
 
-                <ul className="nav nav-pills">
-                    <li className="nav-item">
-                    <a className="nav-link" href="#Market_Cap_Descending" onClick={this.sortByRankDesc}>
-                        <i className="fas fa-sort-amount-up"></i>
-                        <i className="fas fa-sync fa-spin"></i>
-                        <span> Market Cap</span>
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#Price_Ascending" onClick={this.sortByPriceAsc}>
-                        <i className="fas fa-sort-amount-down fa-flip-vertical"></i>
-                        <i className="fas fa-dollar-sign"></i>
-                        <span> Lowest Price USD</span>
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#One_Hour_Change_Descending" onClick={this.sortByChangeHour}>
-                        <i className="fas fa-sort-amount-up"></i>
-                        <i className="far fa-clock"></i>
-                        <span> 1hr Most Change</span>
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#24-Hour_Change_Descending" onClick={this.sortByChangeDay}>
-                        <i className="fas fa-sort-amount-up"></i>
-                        <i className="fas fa-sun"></i>
-                         <span> 24hr Most Change</span>
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#Week_Change_Descending" onClick={this.sortByChangeWeek}>
-                        <i className="fas fa-sort-amount-up"></i>
-                        <i className="fas fa-calendar-alt"></i>
-                        <span> Weekly Most Change</span>
-                    </a>
+                <ul className="nav nav-pills fixed-top">
+
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle btn" data-toggle="dropdown" href="#!" role="button" aria-haspopup="true" aria-expanded="false">Filters</a>
+                        <div className="dropdown-menu dropdown-menu-right">
+                        
+                        <a href="#top" className="dropdown-item">
+                            <i className="fas fa-arrow-up"></i>
+                            <span> Return to Top</span>
+                        </a>
+
+                        <a href="#Market_Cap_Descending" className="dropdown-item" onClick={this.sortByRankDesc}>
+                            <i className="fas fa-sort-amount-up"></i>
+                            <i className="fas fa-sync fa-spin"></i>
+                            <span> Market Cap</span>
+                        </a>
+                        <a href="#Price_Ascending" className="dropdown-item" onClick={this.sortByPriceAsc}>
+                            <i className="fas fa-sort-amount-down fa-flip-vertical"></i>
+                            <i className="fas fa-dollar-sign"></i>
+                            <span> Lowest Price USD</span>
+                        </a>
+                        <a href="#One_Hour_Change_Descending" className="dropdown-item" onClick={this.sortByChangeHour}>
+                            <i className="fas fa-sort-amount-up"></i>
+                            <i className="far fa-clock"></i>
+                            <span> 1hr Change</span>
+                        </a>
+                        <a href="#24-Hour_Change_Descending" className="dropdown-item" onClick={this.sortByChangeDay}>
+                            <i className="fas fa-sort-amount-up"></i>
+                            <i className="fas fa-sun"></i>
+                             <span> 24hr Change</span>
+                        </a>
+                        <a href="#Week_Change_Descending" className="dropdown-item" onClick={this.sortByChangeWeek}>
+                            <i className="fas fa-sort-amount-up"></i>
+                            <i className="fas fa-calendar-alt"></i>
+                            <span> Weekly Change</span>
+                        </a>
+
+                        </div>
                     </li>
                 </ul>
-
 
 
 
@@ -130,8 +139,8 @@ class App extends React.Component {
                 { this.state.coins.map(coin =>
                     <a href={"https://coinmarketcap.com/currencies/" + coin.id}>
 
+
                     <div className={"media-shift card m-4 container-fluid card-bg-img bg-" + coin.id}>
-                        
                         {/* title zone */}
                         <div className="mt-2 card-header align-items-center justify-content-around">
                             <span className="h3">#{coin.rank} </span>
@@ -141,7 +150,6 @@ class App extends React.Component {
                             <span className="h3">({coin.symbol})</span>
                         </div>
 
-                        
                         {/* metadata zone */}
                         <span className="text-center h4 price">
                             Value in USD $ 
@@ -149,22 +157,22 @@ class App extends React.Component {
                         </span>
 
                         <div className="list-group flex-column">
-                            <div className="list-group-item d-flex justify-content-between align-items-center hourly">
-                                <span>
+                            <div className="list-group-item d-flex flex-row justify-content-between align-items-center hourly">
+                                <span className="">
                                     <i className="pr-2 far fa-clock"></i>
                                      Hourly:
                                 </span>
                                 <h5> {coin.percent_change_1h}%</h5>
                             </div>
-                            <div className="list-group-item d-flex justify-content-between align-items-center daily">
-                                <span>
+                            <div className="list-group-item d-flex flex-row justify-content-between align-items-center daily">
+                                <span className="" >
                                     <i className="pr-2 fas fa-sun"></i>
                                      Daily:
                                 </span>
                                 <h5> {coin.percent_change_24h}%</h5>
                             </div>
-                            <div className="list-group-item d-flex justify-content-between align-items-center weekly">
-                                <span>
+                            <div className="list-group-item d-flex flex-row justify-content-between align-items-center weekly">
+                                <span className="">
                                     <i className="pr-2 fas fa-calendar-alt"></i>
                                      Weekly:
                                 </span>
@@ -172,8 +180,8 @@ class App extends React.Component {
                             </div>
                         </div>
 
-                    </div>
 
+                    </div>
                     </a>
                 )}
             </div>
