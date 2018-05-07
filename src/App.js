f3f2051df1de9f6af227f66eea0d33ca4ddabb55
Adjectival <alexd.jacks@gmail.com>
2018-05-07 10:51:53 -0700

    :sparkles: new page scroll btns in sticky navbar, new mktcap metadata

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import axios from 'axios';
-
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'mdbreact';
+import NumberFormat from 'react-number-format';
 
 import './App.css';
-
 
 class App extends React.Component {
 
     constructor(props) {   
         super(props)
         this.state = {
-            coins: []
+            coins: [],
+            NowTS: Date.now(),
+            FortnightlyBeforeNow: Date.now()-1382434
         }
         this.sortByRankDesc = this.sortByRankDesc.bind(this);
         this.sortByPriceAsc = this.sortByPriceAsc.bind(this);
@@ -21,17 +22,23 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        axios.get('https://api.coinmarketcap.com/v1/ticker/?start=0&limit=99')
-            .then(res => {
-                const coins = res.data;
-                this.setState({ coins });
+        
+        console.log("Now TS: "+ this.state.NowTS);
+        console.log("2 weeks ago TS: "+ this.state.FortnightlyBeforeNow);
+        
+        Promise.all([
+            axios.get('https://api.coinmarketcap.com/v1/ticker/?start=0&limit=200')
+            ])
+            .then(([ response ]) => {
+                this.setState({ coins: response.data });
+
+                console.log(this.state.coins);
             });
-        axios.get()
-            .then(res => {
-                const history = res.data;
-                this.setState({ history });
-            });
+
+        const fortnight_element = <h5>fortnite test</h5>;
+        ReactDOM.render(fortnight_element, document.getElementById('fortnight'));
     }
+
 
     sortByRankDesc() {
         this.setState(prevState => {
@@ -65,67 +72,86 @@ class App extends React.Component {
     }
 
     render() {
+        const { coins, FortnightlyBeforeNow } = this.state
         return (
         <div className="">
             <div className="header-shading p-3 text-left" id="top">
 
-                <h1 className="display-5">Top 99 Crypto Currencies</h1>
-                <h2 className="display-6">Updated every 5 minutes <a href="https://coinmarketcap.com/all/views/all/">via</a></h2>
+                <h1 className="pt-5">Top 200 Crypto Currencies</h1>
+                <h3 className="">Updated every 5 minutes <a href="https://coinmarketcap.com/all/views/all/">via coinmarketcap.com</a></h3>
                 <h6>
                     Created for educational purposes only by <a href="https://github.com/Adjectival">Alexander Jacks</a>.
                 </h6>
                 <h6>
                     Not intended as a replacement for professional investment advice.
                 </h6>
+                <h5>
+                    Upcoming coins may not have proper images yet-- helps flag them as climbers! 
+                </h5>
                 <h4>
                     Want a crypto guide?
                     <a href="https://how-to-crypto.herokuapp.com/"> Check this out.</a>
                 </h4>
                 <h6 className="donate">
-                    Donate ETH: 0xd74cb5c0e868B06DaEA6Bb25364dCF1f2C096855
+                    
+                    <span> Donate ETH <i className="fab fa-ethereum"></i>: 0xd74cb5c0e868B06DaEA6Bb25364dCF1f2C096855</span>
+                </h6>
+                <h6 className="donate">
+                    <span> Donate BTC <i className="fab fa-bitcoin"></i>: 12YpKeoQ7VToosj9MoQpcM6srAbnxs42gq</span>
                 </h6>
 
 
-                <ul className="nav nav-pills fixed-top justify-content-center">
+                <div className="btn-group fixed-top justify-content-center" role="group" aria-label="Navigation button group with dropdown">
 
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle btn" data-toggle="dropdown" href="#!" role="button" aria-haspopup="true" aria-expanded="false">Filters</a>
-                        <div className="dropdown-menu dropdown-menu-right">
-                        
-                        <a href="#top" className="dropdown-item">
-                            <i className="fas fa-arrow-up"></i>
-                            <span> Return to Top</span>
-                        </a>
+                    <button type="button" className="btn-lg">
+                    <a href="#top" className="arrow-btn top-btn">
+                        <i className="fas fa-arrow-up"></i>
+                        <span> Top of List</span>
+                    </a>
+                    </button>
 
-                        <a href="#Market_Cap_Descending" className="dropdown-item" onClick={this.sortByRankDesc}>
-                            <i className="fas fa-sort-amount-up"></i>
-                            <i className="fas fa-sync fa-spin"></i>
-                            <span> Market Cap</span>
-                        </a>
-                        <a href="#Price_Ascending" className="dropdown-item" onClick={this.sortByPriceAsc}>
-                            <i className="fas fa-sort-amount-down fa-flip-vertical"></i>
-                            <i className="fas fa-dollar-sign"></i>
-                            <span> Lowest Price USD</span>
-                        </a>
-                        <a href="#One_Hour_Change_Descending" className="dropdown-item" onClick={this.sortByChangeHour}>
-                            <i className="fas fa-sort-amount-up"></i>
-                            <i className="far fa-clock"></i>
-                            <span> 1hr Change</span>
-                        </a>
-                        <a href="#24-Hour_Change_Descending" className="dropdown-item" onClick={this.sortByChangeDay}>
-                            <i className="fas fa-sort-amount-up"></i>
-                            <i className="fas fa-sun"></i>
-                             <span> 24hr Change</span>
-                        </a>
-                        <a href="#Week_Change_Descending" className="dropdown-item" onClick={this.sortByChangeWeek}>
-                            <i className="fas fa-sort-amount-up"></i>
-                            <i className="fas fa-calendar-alt"></i>
-                            <span> Weekly Change</span>
-                        </a>
-
+                    <div className="btn-group" role="group">
+                        <button id="btnGroupDropdown" type="button" className="btn-lg btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Filters
+                        </button>
+                        <div className="dropdown-menu" aria-labelledby="btnGroupDropdown">
+                            <a href="#Market_Cap_Descending" className="dropdown-item" onClick={this.sortByRankDesc}>
+                                <i className="fas fa-sort-amount-up"></i>
+                                <i className="fas fa-sync"></i>
+                                <span> Market Cap</span>
+                            </a>
+                            <a href="#Price_Ascending" className="dropdown-item" onClick={this.sortByPriceAsc}>
+                                <i className="fas fa-sort-amount-down fa-flip-vertical flip-vertical-fix"></i>
+                                <i className="fas fa-dollar-sign"></i>
+                                <span> Lowest Price</span>
+                            </a>
+                            <a href="#One_Hour_Change_Descending" className="dropdown-item" onClick={this.sortByChangeHour}>
+                                <i className="fas fa-sort-amount-up"></i>
+                                <i className="far fa-clock"></i>
+                                <span> 1hr Change</span>
+                            </a>
+                            <a href="#24-Hour_Change_Descending" className="dropdown-item" onClick={this.sortByChangeDay}>
+                                <i className="fas fa-sort-amount-up"></i>
+                                <i className="fas fa-sun"></i>
+                                 <span> 24hr Change</span>
+                            </a>
+                            <a href="#Week_Change_Descending" className="dropdown-item" onClick={this.sortByChangeWeek}>
+                                <i className="fas fa-sort-amount-up"></i>
+                                <i className="fas fa-calendar-alt"></i>
+                                <span> Weekly Change</span>
+                            </a>
                         </div>
-                    </li>
-                </ul>
+                    </div>
+
+                    <button type="button" className="btn-lg">
+                    <a href="#bottom" className="bottom-btn arrow-btn">
+                        <i className="fas fa-arrow-down"></i>
+                        <span> Bottom of List</span>
+                    </a>
+                    </button>
+
+                </div>
+
 
 
 
@@ -136,18 +162,22 @@ class App extends React.Component {
 
 {/* iterate thru the array list gathered from the API*/}
             <div className="card-deck justify-content-around">
-                { this.state.coins.map(coin =>
+                { coins.map(coin =>
                     <a href={"https://coinmarketcap.com/currencies/" + coin.id}>
 
 
-                    <div className={"media-shift card m-4 container-fluid card-bg-img bg-" + coin.id}>
+                    {/* each card */}
+                    <div className={"media-shift card card-shadow m-4 container-fluid card-bg-img bg-" + coin.id}>
                         {/* title zone */}
                         <div className="mt-2 card-header align-items-center justify-content-around">
                             <span className="h3">#{coin.rank} </span>
 
-                            <img className="sm-icon" src={"./images/"+coin.id+".png"} />
+                            <img className="sm-icon" src={"./images/" + coin.id + ".png"} />
                             <span className="h3"> {coin.name} </span>
                             <span className="h3">({coin.symbol})</span>
+                            <p className="text-right mb-0">
+                                MktCapUSD: {coin.market_cap_usd}
+                            </p>
                         </div>
 
                         {/* metadata zone */}
@@ -178,16 +208,25 @@ class App extends React.Component {
                                 </span>
                                 <h4> {coin.percent_change_7d}%</h4>
                             </div>
+                            <div className="list-group-item d-flex flex-row justify-content-between align-items-center weekly">
+                                <span className="">
+                                    <i className="pr-2 fab fa-fort-awesome"></i>
+                                     Fortnightly:
+                                </span>
+
+                                <h5>{coin.symbol + FortnightlyBeforeNow}</h5>
+
+                            </div>
                         </div>
-
-
+                    
                     </div>
+                    {/* end card */}
                     </a>
                 )}
             </div>
             {/* end iteration */}
-
-    </div>
+        <span id="bottom"></span>
+        </div>
         );
     }
 }
